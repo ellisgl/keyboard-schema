54cb172a6234cabafc27412528c3bcc3085f94e2
ellisgl <xelliss@gmail.com>
2020-07-26 00:39:59 -0500

    Comment fix.

--- switch.ts
+++ switch.ts
@@ -1,5 +1,5 @@
 export interface Switch {
     style: 'mx' | 'alps'
     pins: 2 | 3 | 5
-    hasRGB: boolean      // Does this switch has hold for RGB led?
+    hasRGB: boolean      // Does this switch have holes for RGB led?
 }
