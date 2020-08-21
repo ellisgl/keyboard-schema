a5b55ccb9e7be8b31d70a9b5b2d2a73f5c021589
ellisgl <xelliss@gmail.com>
2020-08-21 17:40:23 -0500

    Removed the TS interface definitions. Added some extra enum stuff, fixed a typo. Moved some stuff around.

--- board.ts
+++
@@ -1,8 +0,0 @@
-import { Key } from "./key";
-import { Accessory } from "./accessory";
-
-export interface Board {
-    name: string
-    keys: Key[]
-    accessories: Accessory[]
-}
--- switch.ts
+++
@@ -1,5 +0,0 @@
-export interface Switch {
-    style: 'mx' | 'alps'
-    pins: 2 | 3 | 5
-    hasRGB: boolean      // Does this switch have holes for RGB led?
-}
